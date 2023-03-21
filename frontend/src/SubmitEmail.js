216eb05ce3f2f876f1e8cd40b62268a8f8e888c3
Emil <113785577+GrustnyyVeselchak@users.noreply.github.com>
2023-03-21 21:31:42 +0400

    SubmitEmail
    Вроде бы отправляет, а что - хуй знает. [обджект-хуебджект]

--- frontend/src/SubmitEmail.js
+++ frontend/src/SubmitEmail.js
@@ -1,23 +1,36 @@
-import {React,  useEffect} from "react";
+import {React, useState} from "react";
 import axios from "axios";
 
 
 export default function SumbitEmail(props) {
-    
-    useEffect(()=>(
+    const [email, setEmail] = useState({
+        email: ''
+    })
+    const handleInput = (event) => {
+        setEmail({...email, [event.tartget.name]: event.tartget.value})
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        console.log(email)
         axios
-            .post("/api/submit/:email")
+            .post(`/api/submit/:email`, {email})
             .then(response=>{
                 console.log(response);
-            })   
-    ));
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
 
     return(
         <div>
-            <form>
-                <input
+            <form onSubmit={handleSubmit}>
+                email: <input
                     type="text"
-                    />
+                    onChange={handleInput}
+                    name="email"
+                    /> 
                 <input
                     type="submit"/>
             </form>
