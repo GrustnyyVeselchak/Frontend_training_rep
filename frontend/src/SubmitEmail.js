75fc20d35f4fc5134b45acb0194a895ae7b611c1
Emil <113785577+GrustnyyVeselchak@users.noreply.github.com>
2023-03-22 22:02:22 +0400

    почти готовый SubmitEmail
    уведомления всплывают, но в каком то сером контейнере. Решать сейчас проблему не хочу, уже заебался. Будущий Я, рассчитываю на тебя

--- frontend/src/SubmitEmail.js
+++ frontend/src/SubmitEmail.js
@@ -1,9 +1,19 @@
-import {React, useState} from "react";
+import { React, useState} from "react";
 import axios from "axios";
+
+import { Alert, Snackbar } from "@mui/material";
+
 
 
 export default function SumbitEmail(props) {
     const [email, setEmail] = useState('');
+    const [status, setStatus] = useState('');
+    const [err, setErr] = useState('');
+    const [open, setOpen] = useState(false);
+
+    const handleInput = (event) => {
+        setEmail(event.target.value)
+    };
 
     const handleSubmit = async (event) => {
         event.preventDefault();
@@ -11,17 +21,49 @@ export default function SumbitEmail(props) {
         await axios
             .post(`/api/submit/${email}`)
             .then(response=>{
-                console.log(response);
+                setStatus(response.data.message);
             })
             .catch(error => {
-                console.log(error);
+                setErr('error ebta blat')
             })
     };
-    
-    const handleInput = (event) => {
-        setEmail(event.target.value)
-    };
 
+    const handleClick = () => {
+        setOpen(true);
+      };
+
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+          return;
+        }
+    
+        setOpen(false);
+      };
+
+      const action = (
+        <div>
+            {status ? 
+                        <Alert 
+                            variant="filled" 
+                            severity="success" 
+                            onClose={handleClose}
+                            sx={{ width: '100%' }}
+                        >
+                            {status}
+                        </Alert>
+                    : 
+                        <Alert 
+                            variant="filled" 
+                            severity="warning" 
+                            onClose={handleClose}
+                            sx={{ width: '100%' }}
+                        >
+                            {err}
+                        </Alert>}
+        </div>
+            
+      );
+    
     return(
         <div>
             <form onSubmit={handleSubmit}>
@@ -32,8 +74,19 @@ export default function SumbitEmail(props) {
                     onChange={handleInput}
                     /> 
                 <input
-                    type="submit"/>
+                    type="submit"
+                    onClick={handleClick}/>
+                    <Snackbar 
+                        open={open} 
+                        autoHideDuration={3000} 
+                        anchorOrigin={{ vertical:'top', horizontal: 'left',  }}
+                        action={action}
+                        onClose={handleClose}
+                        > 
+                    </Snackbar>
             </form>
         </div>
+
+        
     )
 };
